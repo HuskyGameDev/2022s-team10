c4b0da3b263de7a87b0320fe2e7d45869442b785
jkyt07 <71299182+jkyt07@users.noreply.github.com>
2022-01-25 15:54:46 -0500

    Better Controls
    Changed Movement to be velocity based
    Added Wall Jumping

--- Rise of the Damned/Assets/PlayerController.cs
+++ Rise of the Damned/Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed;
+    public int speed;   //speed of player horizontally
     private Rigidbody2D rb;
     private Collider2D thisCollider;
 
@@ -19,14 +19,29 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         int horiz = 0;
-        int vert = 0;
+        //int vert = 0;
         if (Input.GetKey(KeyCode.D))
             horiz += 1;
         if (Input.GetKey(KeyCode.A))
             horiz -= 1;
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
-            vert += 1;
-        Vector3 input = new Vector3(horiz, vert * 30, 0f);
-        transform.Translate(input * speed * Time.deltaTime);
+        
+        /*Vector3 input = new Vector3(horiz, vert * 30, 0f);
+        transform.Translate(input * speed * Time.deltaTime);*/
+        rb.AddForce(Time.deltaTime * speed * new Vector2(horiz, 0).normalized);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+            rb.velocity = rb.velocity - new Vector2(0, rb.velocity.y * Time.deltaTime * .1f);
+
+        List<Collider2D> results = new List<Collider2D>();
+        thisCollider.OverlapCollider(new ContactFilter2D(), results);
+
+        foreach(Collider2D collision in results)
+        if (collision.tag == "Ground")
+        {
+            rb.velocity = rb.velocity - new Vector2(0, rb.velocity.y * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) && rb.velocity.y < 400)
+                            rb.AddForce(new Vector2(rb.velocity.y < 0 ? 1000 * Mathf.Sign(transform.position.x - collision.gameObject.transform.position.x) : 0, 1000));
+        }
+            
     }
 }
