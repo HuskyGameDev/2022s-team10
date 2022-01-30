5c75ba94ed359ba150dd70293b3e4ededecf1a7f
Tyler Poirier <tyler.k.poirier@gmail.com>
2022-01-30 01:08:33 -0500

    Player Model and Hitbox
    Added sprite to player model, updated hitbox, sprite will turn left and right based on movement direction. .gitignore File placed in root

--- Rise of the Damned/Assets/PlayerController.cs
+++ Rise of the Damned/Assets/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     public int speed;   //speed of player horizontally
     private Rigidbody2D rb;
     private Collider2D thisCollider;
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
-       thisCollider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        thisCollider = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -36,12 +38,17 @@ public class PlayerController : MonoBehaviour
         thisCollider.OverlapCollider(new ContactFilter2D(), results);
 
         foreach(Collider2D collision in results)
-        if (collision.tag == "Ground")
-        {
-            rb.velocity = rb.velocity - new Vector2(0, rb.velocity.y * Time.deltaTime);
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) && rb.velocity.y < 400)
-                            rb.AddForce(new Vector2(rb.velocity.y < 0 ? 1000 * Mathf.Sign(transform.position.x - collision.gameObject.transform.position.x) : 0, 1000));
-        }
-            
+            if (collision.tag == "Ground")
+            {
+                rb.velocity = rb.velocity - new Vector2(0, rb.velocity.y * Time.deltaTime);
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) && rb.velocity.y < 400)
+                    rb.AddForce(new Vector2(rb.velocity.y < 0 ? 1000 * Mathf.Sign(transform.position.x - collision.gameObject.transform.position.x) : 0, 1000));
+            }
+
+        // Change sprite X flip status based on whether the player is moving left or right
+        if (Input.GetKey(KeyCode.A))
+            sr.flipX = true;
+        if (Input.GetKey(KeyCode.D))
+            sr.flipX = false;
     }
 }
