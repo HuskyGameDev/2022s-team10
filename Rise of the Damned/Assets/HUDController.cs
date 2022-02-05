494ecdc7c6cb1181b7993f9e2700eb489a3e2244
Tyler Poirier <tyler.k.poirier@gmail.com>
2022-02-04 19:34:40 -0500

    Updated HUD Controller
    Taking damage integration with HUD health bar.

--- Rise of the Damned/Assets/HUDController.cs
+++ Rise of the Damned/Assets/HUDController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HUDController : MonoBehaviour
 {
     private AttackController playerScript;
-    public SpriteRenderer sr;
+    private SpriteRenderer sr;
     public Sprite[] spriteArray;
 
     // Start is called before the first frame update
@@ -19,9 +19,18 @@ public class HUDController : MonoBehaviour
     void Update()
     {
         ChangeSprite();
+        TakeDamage();
     }
 
     void ChangeSprite(){
-        sr.sprite = spriteArray[Mathf.Clamp(0, spriteArray.Length - 1, playerScript.health / 7)];
+        //sr.sprite = spriteArray[Mathf.Clamp(0, spriteArray.Length - 1, playerScript.health / 7)];
+        sr.sprite = spriteArray[Mathf.RoundToInt((float)(playerScript.health) / (float)(playerScript.maxHealth) * 14f)];
+    }
+
+    void TakeDamage()
+    {
+        if (Input.GetKeyDown(KeyCode.G) || ){
+            playerScript.health -= 5;
+        }
     }
 }
