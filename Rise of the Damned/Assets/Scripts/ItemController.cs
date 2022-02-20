60d78a445e1f219adbb960225aac6f839c3b873e
Tyler Poirier <tyler.k.poirier@gmail.com>
2022-02-19 20:10:26 -0500

    Item Drop Info
    Dropped items will now display basic a basic attack/defense statistic when the player hovers over (collides with) them.
    
    Dropped items are now Prefabs.

--- Rise of the Damned/Assets/Scripts/ItemController.cs
+++ Rise of the Damned/Assets/Scripts/ItemController.cs
@@ -7,5 +7,39 @@ public class ItemController : MonoBehaviour
 {
     public string type;
     public float damage, bowDamage, armor;
-    
+    private SpriteRenderer sr;
+    private SpriteRenderer[] children;
+    void Start(){
+        sr = GetComponent<SpriteRenderer>();
+        children = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.name.Equals("Player")){
+            Color color = sr.color;
+            color.a = 1.0f;
+            Color childColor;
+            foreach(SpriteRenderer child in children){
+                if (child.tag.Equals("ItemInfo")){
+                childColor = child.color;
+                childColor.a = 1.0f;
+                child.color = color;
+                }
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other){
+        if (other.gameObject.tag.Equals("Player")){
+            Color color = sr.color;
+            color.a = 0.0f;
+            Color childColor;
+            foreach(SpriteRenderer child in children){
+                if (child.tag.Equals("ItemInfo")){
+                    childColor = child.color;
+                    childColor.a = 0.0f;
+                    child.color = color;
+                }
+            }
+        }
+    }
 }
