52474ba63a147eb56b7cc6c621d3a2a4f75dff62
Chris Apfel <97426721+ChrisA115@users.noreply.github.com>
2022-02-22 15:05:00 -0500

    small walljump animation fix, spike hitbox fix
    also added fluid grid

---
+++ Rise of the Damned/Assets/Scripts/Lava.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+
+    public float knockback;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag.Equals("Player")) { //if player collides with lava
+            PlayerController.health = PlayerController.health - 10;
+            //knockback
+            collision.rigidbody.velocity += new Vector2(Mathf.Sign(collision.transform.position.x - transform.position.x) * knockback, knockback / 2);
+        }
+    }
+
+}
