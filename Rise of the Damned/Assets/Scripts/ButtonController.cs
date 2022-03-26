cdf202dbb985e65f279c587fe67cc7dd7141e3d5
Tyler Poirier <tyler.k.poirier@gmail.com>
2022-03-26 14:48:05 -0400

    Game Over Complete
    Game Over menu is now fully complete and allows for restarting the game or returning to the (yet to be implemented) main menu.

--- Rise of the Damned/Assets/Scripts/ButtonController.cs
+++ Rise of the Damned/Assets/Scripts/ButtonController.cs
@@ -8,9 +8,36 @@ public class ButtonController : MonoBehaviour
 {
     public static GameObject buttonClick;
 
+    public bool isSelected;
+
+    private SpriteRenderer sr;
+
+    void Start(){
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     void Update(){
-        if (Input.GetKeyDown(KeyCode.R)){
-            SceneManager.LoadScene("LevelOne");
+        if (isSelected){
+            Color color = sr.color;
+            color.a = 1.0f;
+            sr.color = color;
+        }
+        else {
+            Color color = sr.color;
+            color.a = 0.0f; 
+            sr.color = color;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+            isSelected = !isSelected;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)){
+            if (gameObject.name.Equals("SelectRetry") && isSelected){
+                SceneManager.LoadScene("LevelOne");
+            } else {
+                Debug.Log("Main menu here");
+            }
         }
     }
 }
