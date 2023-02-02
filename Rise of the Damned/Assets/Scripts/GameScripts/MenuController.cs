619ec33e762d6114246c9a638fffd85807e76669
Tyler Poirier <tyler.k.poirier@gmail.com>
2023-02-01 23:31:40 -0500

    Fullscreen and Resolution Options
    - Added simple fullscreen option to allow toggling between windowed and fullscreen. Windowed currently doesn't let you maximize the window, don't know what that's about.
    - Added big chunk of code to grab player's monitor resolutions to add to a list of options to pick from and set game resolution to, including separate refresh rates for each resolution which will be a separate option in the future (or not an option at all)

--- Rise of the Damned/Assets/Scripts/GameScripts/MenuController.cs
+++ Rise of the Damned/Assets/Scripts/GameScripts/MenuController.cs
@@ -2,9 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    void Start(){
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        int currentResolutionIndex = 0;
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++){
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+            if (resolutions[i].width  == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height ){
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void ToggleFullscreen(bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
+    }
+
     public void GameOverRetry(){
         Time.timeScale = 1;
         PlayerController.isPaused = false;
