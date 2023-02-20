4b88c5f1679a3b43b8557c0e0d33acebb6112376
tjbrewer <brewertj11@gmail.com>
2023-02-20 12:13:37 -0500

    Reaper animations
    Added attack and movement animations to the reaper. Also made it bigger and more menacing.

--- Rise of the Damned/Assets/Scripts/EnemyScripts/Reaper.cs
+++ Rise of the Damned/Assets/Scripts/EnemyScripts/Reaper.cs
@@ -8,6 +8,15 @@ public class Reaper : EnemyController
     private Coroutine stateUpdate; //calls the coroutine for the state of the enemy
     [SerializeField]
     bool isAttacking = true;
+    bool isIdle = false;
+    bool isSwinging = false;
+    bool isSwinging2 = false; //second half of animation for knockback cancel
+    bool lockSwing = false; //locks the state to swinging to prevent half swings
+
+    [SerializeField]
+    float swingDistance = 2f;
+    [SerializeField]
+    float attackCooldown = 1f;
 
     [Header("Animations")]
     public Animator animator;
@@ -18,15 +27,27 @@ public class Reaper : EnemyController
         {
             state = defaultState;
         } else if (Vector2.Distance(rb.position, PlayerController.controller.rb.position) < aggroDist && !isAttacking //far enough away from player
-              && Mathf.Abs(rb.position.x - PlayerController.controller.rb.position.x) > .5) //and not under or over the player
+              && Vector2.Distance(rb.position, PlayerController.controller.rb.position) > swingDistance) //and not under or over the player
         {
             state = State.Attack;
+        } else if (Vector2.Distance(rb.position, PlayerController.controller.rb.position) < swingDistance)
+        {
+            state = State.Swing;
         }
     }
 
     public override void Wander()
     {
+        if (!isIdle && !lockSwing) // is not already idle and not currently swinging
+        {
+            isIdle = true;
+            isAttacking = false;
+            isSwinging = false;
+            if (stateUpdate != null) { StopCoroutine(stateUpdate); }
+            stateUpdate = StartCoroutine(Idle());
+        }
 
+        /*
         if (isAttacking)
         {
             isAttacking = false;
@@ -37,17 +58,41 @@ public class Reaper : EnemyController
             && Mathf.Abs(rb.position.x - PlayerController.controller.rb.position.x) > .5) //and not under or over the player
         {
             state = State.Attack;
-        }
+        } */
     }
 
     public override void Attack()
     {
+        if (!isAttacking && !lockSwing) // is not already attacking and not currently swinging
+        {
+            isAttacking = true;
+            isIdle = false;
+            isSwinging = false;
+            if (stateUpdate != null) { StopCoroutine(stateUpdate); }
+            stateUpdate = StartCoroutine(Agro());
+        }
+
+
+        /*
         if (!isAttacking)
         {
             isAttacking = true;
             if (stateUpdate != null) { StopCoroutine(stateUpdate); }
             stateUpdate = StartCoroutine(Agro());
+        } */
+    }
+
+    public override void Swinging()
+    {
+        if (!isSwinging)
+        {
+            isSwinging = true;
+            isIdle = false;
+            isAttacking = false;
+            if (stateUpdate != null) { StopCoroutine(stateUpdate); }
+            stateUpdate = StartCoroutine(Swing());
         }
+
     }
 
     IEnumerator Idle()
@@ -56,7 +101,7 @@ public class Reaper : EnemyController
         {
             bool hitWall = false;
             //beginning animation
-            //animator.SetBool("isRunning", false); //idle for 6 seconds
+            animator.SetBool("isMoving", false); //idle for 6 seconds
             rb.velocity = (new Vector2(0, 0)); //stops moving
             yield return new WaitForSeconds(Random.Range(2, 5));
 
@@ -66,7 +111,7 @@ public class Reaper : EnemyController
             }
 
             //move
-            //animator.SetBool("isRunning", true); //running for 1 second
+            animator.SetBool("isMoving", true); //running for 1 second
 
             while (!hitWall) // loop until hit a wall or edge
             {
@@ -98,7 +143,7 @@ public class Reaper : EnemyController
         while (true)
         {
             //start animation
-            //animator.SetBool("isRunning", true);
+            animator.SetBool("isMoving", true);
 
             //start jumping part of animation
 
@@ -113,7 +158,10 @@ public class Reaper : EnemyController
 
             if (!receivingKnockback)
             {
-                velocity.x = Mathf.MoveTowards(velocity.x, desired_velocity.x, max_speed_change);
+                if(!CheckWall()) //if is not running into a wall (so it doesn't get stuck on a wall
+                {
+                    velocity.x = Mathf.MoveTowards(velocity.x, desired_velocity.x, max_speed_change);
+                }
                 velocity.y = Mathf.MoveTowards(velocity.y, desired_velocity.y, max_speed_change);
             }
 
@@ -129,12 +177,44 @@ public class Reaper : EnemyController
 
     }
 
+    IEnumerator Swing()
+    {
+        while (true)
+        {
+            if (!receivingKnockback)
+            {
+                FacePlayer();
+
+                //animator.SetBool("isRunning", false); //stops running -> to idle animation
+
+                rb.velocity = (new Vector2(0, 0)); //stops moving
+
+                animator.SetBool("isAttacking", true); // starts swinging animation
+                lockSwing = true;
+
+                yield return new WaitForSeconds(.36f);
+
+                isSwinging2 = true;
+
+                yield return new WaitForSeconds(.24f);
+
+                animator.SetBool("isAttacking", false); // stops swinging animation
+                lockSwing = false;
+                isSwinging2 = false;
+
+                yield return new WaitForSeconds(attackCooldown); // wait for attack cooldown 
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
     public override void FacePlayer()
     {
         Vector3 rotation = transform.eulerAngles;
         direction.x = (int)Mathf.Sign(PlayerController.player.transform.position.x - transform.position.x);
-        if (PlayerController.player.transform.position.y - transform.position.y < -.1f &&
-            PlayerController.player.transform.position.y - transform.position.y > -.4f)
+        if (PlayerController.player.transform.position.y - transform.position.y < -.3f &&
+            PlayerController.player.transform.position.y - transform.position.y > -.6f)
         {
             direction.y = 0;
         } else
